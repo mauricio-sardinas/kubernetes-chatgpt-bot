2b18cb59705f32e8b758bde83ddeed40229ac004
Mauricio Sardinas <mauricio.sardinas@cbrands.com>
2023-02-23 15:33:23 -0600

    update inital enrichment

--- chatgpt_robusta_actions/chat_gpt.py
+++ chatgpt_robusta_actions/chat_gpt.py
@@ -60,25 +60,61 @@ def show_chat_gpt_search(event: ExecutionBaseEvent, params: ChatGPTParams):
 
 @action
 def chat_gpt_enricher(alert: PrometheusKubernetesAlert, params: ChatGPTTokenParams):
+    # """
+    # Add a button to the alert - clicking it will ask chat gpt to help find a solution.
+    # """
+    # alert_name = alert.alert.labels.get("alertname", "")
+    # if not alert_name:
+    #     return
+
+    # alert.add_enrichment(
+    #     [
+    #         CallbackBlock(
+    #             {
+    #                 f'Ask ChatGPT': CallbackChoice(
+    #                     action=show_chat_gpt_search,
+    #                     action_params=ChatGPTParams(
+    #                         search_term=f"How to solve {alert_name} on Kubernetes?",
+    #                         chat_gpt_token=params.chat_gpt_token,
+    # ),
+    #                 )
+    #             },
+    #         )
+    #     ]
+    # )
     """
-    Add a button to the alert - clicking it will ask chat gpt to help find a solution.
+    Add a finding with ChatGPT top results for the specified search term.
+    This action can be used together with the stack_overflow_enricher.
     """
     alert_name = alert.alert.labels.get("alertname", "")
     if not alert_name:
         return
 
-    alert.add_enrichment(
-        [
-            CallbackBlock(
-                {
-                    f'Ask ChatGPT': CallbackChoice(
-                        action=show_chat_gpt_search,
-                        action_params=ChatGPTParams(
-                            search_term=f"How to solve {alert_name} on Kubernetes?",
-                            chat_gpt_token=params.chat_gpt_token,
-    ),
-                    )
-                },
-            )
-        ]
+    search_term = f"How to solve {alert_name} on Kubernetes?"
+
+    openai.api_key = params.chat_gpt_token
+    res: OpenAIObject = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=search_term,
+        max_tokens=1000,
+        temperature=0
     )
+
+
+    answers = []
+    if res:
+        for choice in res.choices:
+            answers.append(choice.text)
+
+    if answers:
+        alert.add_enrichment([MarkdownBlock(f"ChatGPT Results *{params.search_term}*")])
+        alert.add_enrichment([ListBlock(answers)])
+    else:
+        alert.add_enrichment(
+            [
+                MarkdownBlock(
+                    f'Sorry, ChatGPT doesn\'t know anything about "{search_term}"'
+                )
+            ]
+        )
+    
